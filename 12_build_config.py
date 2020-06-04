e41cc646ca772f8f1c1d31b2855d24faae8d12a0
CWiswell-IDM <cwiswell@idmod.org>
2020-06-04 11:35:57 -0700

    More like ideal, rather than actual, for config generation

--- 12_build_config.py
+++ 12_build_config.py
@@ -1,6 +1,8 @@
 from emod_api.config import default_from_schema
-import sys
 
-sys.argv = {}
-sys.argv[1] = "schema.json"
-default_from_schema.write_default_from_schema()
+# import sys
+# sys.argv = {}
+# sys.argv[1] = "schema.json"
+
+default_from_schema.write_default_from_schema(schema_name="schema.json")
+
